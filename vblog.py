8e7af6091d5c3b893b61fdd07e32b110a3c0de85
vinaypursnan <vinaypursnani@icloud.com>
2022-10-25 14:34:31 -0500

    Added basic HTML render files for app's decorators

--- vblog.py
+++ vblog.py
@@ -1,15 +1,31 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
+posts = [{
+    'author': 'Vinay Pursnani',
+    'posted_on:': '10/24/2022',
+    'blog_content': 'Portfolio using Flask'
+},
+    {
+        'author': 'Varun Pursnani',
+        'posted_on:': '10/25/2022',
+        'blog_content': 'Checking future progress'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
+
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
